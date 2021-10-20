a2a0e4772a535eff3056b35ab39eb3575fdecbe8
loobiish <kumar.lavish.0109@gmail.com>
2021-10-20 16:25:34 +0530

    Updated model

--- patent_score.py
+++ patent_score.py
@@ -1,16 +1,10 @@
-from sklearn.feature_extraction.text import CountVectorizer
 import joblib as joblib
 import random
-import numpy as np
 
-vec = CountVectorizer(stop_words='english')
-joblib_file = "joblib_model.pkl"
-joblib_model = joblib.load(joblib_file)
+patent_score = "patent_score.pkl"
+joblib_model = joblib.load(patent_score)
 
-x = ['OpenCV Project', 'Smart Cleaning Tool', 'Green India', 'Hard Chipset',
- 'Computer Network and System', 'Doorknobs', 'Smart Energy Meter']
 
-x = vec.fit_transform(x).toarray()
-score = joblib_model.predict(vec.transform([input()]))[0]
+score = joblib_model.predict([input()])[0]
 
 print(round(random.uniform(score-4, score+4), 2))
